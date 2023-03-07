d1f6fcc1866fdd4c86abd43168865509e4b3f557
Samuel Kim <samuelskim420@gmail.com>
2023-03-07 21:46:57 +0000

    finally created that damn ul

--- ticTacToe/src/index.js
+++ ticTacToe/src/index.js
@@ -1,6 +1,11 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view")// require appropriate file
+const Game = require("../ttt_node/game.js")// require appropriate file
+window.game = Game;
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const figure = document.querySelector("figure.ttt") //arraylike object
+  const game = new Game();
+  const view = new View(game, figure)
+
 });
--- ticTacToe/src/ttt-view.js
+++ ticTacToe/src/ttt-view.js
@@ -1,7 +1,22 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el; //Node/Element
+    this.setupBoard();
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    debugger;
+    const ul = document.createElement("ul");
+    for(let x = 0; x < 3; x++) {
+      for(let y = 0; y < 3; y++) {
+        const li = document.createElement("li");
+        li.dataset.pos = JSON.stringify([x,y]);
+        ul.append(li);
+      }
+    }
+    (this.el).append(ul);
+  }
   
   bindEvents() {}
 
